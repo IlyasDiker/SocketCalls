6fb699aa61a2fcdc9f9c9ea0f476ee91a4d9f5f6
IlyasDiker <ilyasbenhssine@gmail.com>
2022-01-29 11:12:10 +0100

    add onConnect listener

--- public/js/app.js
+++ public/js/app.js
@@ -1,29 +1,55 @@
 const chatForm = document.querySelector('#chat-form');
 const chatBox = document.querySelector('#chat-box-messages');
+const usersBox = document.querySelector('#chat-box-header');
 const messageTemplate = document.querySelector('#message-template');
+const userTemplate = document.querySelector('#user-template');
 
+const selfUser = {
+    id: '',
+    name: 'Guest',
+}
+
+let username = window.prompt("Enter a username");
+
+selfUser.name = username;
 const socket = io();
 
-socket.on('message', (message)=>{
-    console.log('%capp.js line:4 message', 'color: #007acc;', message);
-})
-
-socket.on('chatMessage', (data)=>{
-    console.log('%capp.js line:10 data', 'color: #007acc;', data);
-
-    let msgContainer = messageTemplate.content.cloneNode(true);
-    console.log(msgContainer.querySelector('#msg-value'));
-    msgContainer.querySelector('#msg-value').textContent = data;
-    chatBox.prepend(msgContainer);
-})
-
-chatForm.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    let input = e.target.elements.msg
-    let msg = input.value;
-    input.value = '';
-
+setTimeout(()=>{
     
+    socket.on("connect", () => {
+        console.log(socket.id);
+        selfUser.id = socket.id;
+    
+        socket.emit("userJoined", selfUser);
+    });
+    
+    socket.on('message', (message)=>{
+        console.log('%capp.js line:4 message', 'color: #007acc;', message);
+    })
+    
+    socket.on('userJoined', (user)=>{
+        console.log(user);
+        let userContainer = userTemplate.content.cloneNode(true);
+        console.log(userContainer.querySelector('#user-bubble'));
+        userContainer.querySelector('#user-bubble').innerHTML = user.name;
+        console.log(usersBox);
+        usersBox.prepend(userContainer);
+    })
+    
+    socket.on('chatMessage', (data)=>{
+        let msgContainer = messageTemplate.content.cloneNode(true);
+        msgContainer.querySelector('#msg-value').textContent = data;
+        chatBox.prepend(msgContainer);
+    })
+    
+    chatForm.addEventListener('submit', (e)=>{
+        e.preventDefault();
+        let input = e.target.elements.msg;
+        let msg = input.value;
+        input.value = '';
+    
+        socket.emit('chatMessage', msg);
+    })
 
-    socket.emit('chatMessage', msg);
 })
+
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 
-
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
@@ -14,7 +13,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', socket => {
     socket.emit('message', 'Welcome');
-    socket.broadcast.emit('message', 'user connected');
+
+    socket.on('userJoined', (user)=>{
+        io.emit('userJoined', user);
+    })
+    
     socket.on('disconnect', ()=>{
         io.emit('message', 'user left chat');
     })
